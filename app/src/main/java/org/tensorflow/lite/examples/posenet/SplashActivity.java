3ce56b826ca25eac67933f811fedc6d87ccf340e
RIYE <RIYE-KWON>
2021-05-16 23:55:26 +0900

    main

---
+++ app/src/main/java/org/tensorflow/lite/examples/posenet/SplashActivity.java
@@ -0,0 +1,9 @@
+package org.tensorflow.lite.examples.posenet;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/org/tensorflow/lite/examples/posenet/WebViewActivity.java
@@ -0,0 +1,32 @@
+package org.tensorflow.lite.examples.posenet;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebViewActivity extends AppCompatActivity {
+    private WebView mWebView;
+    private WebSettings mWebSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        mWebView = (WebView) findViewById(R.id.webView);
+
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebSettings = mWebView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebSettings.setLoadWithOverviewMode(true);
+        mWebSettings.setUseWideViewPort(true);
+        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        mWebSettings.setDomStorageEnabled(true);
+        mWebView.loadUrl("http://youtube.com");
+
+    }
+}
