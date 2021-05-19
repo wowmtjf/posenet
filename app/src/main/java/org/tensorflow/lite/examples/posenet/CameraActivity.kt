66bdc5b249dcdba17ce9c5d439d83898e244c6d7
yerin <wowmtjf@naver.com>
2021-05-19 17:04:05 +0900

    //

--- app/src/main/java/org/tensorflow/lite/examples/posenet/CameraActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/posenet/CameraActivity.kt
@@ -24,7 +24,6 @@ class CameraActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.tfe_pn_activity_camera)
     window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     savedInstanceState ?: supportFragmentManager.beginTransaction()
       .replace(R.id.container, PosenetActivity())
--- app/src/main/java/org/tensorflow/lite/examples/posenet/MainActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/posenet/MainActivity.kt
@@ -4,25 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.view.View
+import android.view.WindowManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val web_btn=findViewById<Button>(R.id.webview_btn)
+        web_btn.setOnClickListener {
+            setContentView(R.layout.activity_posenet)
+            val youtubeView: WebView =findViewById(R.id.youtubeView)
+            youtubeView.apply{
+                webViewClient= WebViewClient()
+                settings.javaScriptEnabled=true
+            }
+            youtubeView.loadUrl("https://youtube.com")
+            window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+            savedInstanceState ?: supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PosenetActivity())
+                .commit()
+        }
+
+    }
+    override fun onBackPressed() {
+        val youtubeView:WebView =findViewById(R.id.youtubeView)
+        if(youtubeView.canGoBack()){
+            youtubeView.goBack()
+        }else{
+            finish()
+        }
     }
 
-    fun ClickButton1(v: View?) {
-        val intent = Intent(applicationContext, WebViewActivity::class.java)
-        startActivity(intent)
-    }
 
-    fun ClickButton2(v: View?) {
-        val intent = Intent(this@MainActivity, VideoActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun ClickButton3(v: View?) {
-        val intent = Intent(this@MainActivity, ScoreActivity::class.java)
-        startActivity(intent)
-    }
 }
